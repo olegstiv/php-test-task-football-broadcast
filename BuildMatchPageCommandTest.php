0e8b72f013a4d0d6a21e3a473a104fe506d81e62
Kravchuck Oleg <olegstiv1@gmail.com>
2020-03-17 01:49:19 +0300

    add phpunit

---
+++ BuildMatchPageCommandTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Command;
+
+
+class BuildMatchPageCommandTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test__construct()
+    {
+        var_dump(1);
+    }
+}
